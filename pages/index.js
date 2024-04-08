bac383514bf8df84807bf4a3e97f1e1263330ef4
mosalehkamali <mosalehkamali@gmail.com>
2024-04-08 22:59:02 +0430

    create home slider

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 
 export default function Home() {
   return (
     <>
+    <Slider></Slider>
     </>
   );
 }
