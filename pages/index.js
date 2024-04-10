bf43396349a337c8c4fd74a3f52d79746e0b593c
mosalehkamali <mosalehkamali@gmail.com>
2024-04-10 19:30:04 +0430

    create menu

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import About from "@/components/templates/Index/About";
+import Menu from "@/components/templates/Index/Menu";
 import Offer from "@/components/templates/Index/Offer";
 import Services from "@/components/templates/Index/Services";
 import Slider from "@/components/templates/Index/Slider";
@@ -11,17 +12,21 @@ export default function Home({data}) {
       <About></About>
       <Services services={data.services}></Services>
       <Offer></Offer>
+      <Menu  menu={data.menu}></Menu>
     </>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:4000/services")
-  const services = await res.json()
+  const servicesRes = await fetch("http://localhost:4000/services")
+  const services = await servicesRes.json()
+  const menuRes = await fetch("http://localhost:4000/menu")
+  const menu = await menuRes.json()
   return ({
     props: {
       data: {
-        services
+        services,
+        menu
       }
     },
     revalidate: 60*60 *12
