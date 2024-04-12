6ad2d3226ec3e821f8ff502e9484d6b6fb49a9ef
mosalehkamali <mosalehkamali@gmail.com>
2024-04-12 18:30:31 +0430

    create single product comment section

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,12 +2,12 @@ import React from 'react'
 import ProductsDetails from '@/components/templates/Product/ProductDetails'
 import Comments from '@/components/templates/Product/Comments'
 
-function Product({ product }) {
+function Product({ product, comments }) {
 
   return (
     <>
       <ProductsDetails data={product}></ProductsDetails>
-      <Comments></Comments>
+      <Comments comments={comments}></Comments>
     </>
   )
 }
@@ -16,16 +16,23 @@ export default Product
 
 export async function getStaticProps(context) {
   const routeID = context.params.id
-  const res = await fetch('http://localhost:4000/menu')
-  const menuData = await res.json();
+
+  const menuRes = await fetch('http://localhost:4000/menu')
+  const menuData = await menuRes.json();
 
   const product = menuData.find(item => item.id == routeID)
+
+  const commentsRes = await fetch('http://localhost:4000/comments')
+  const commentsData = await commentsRes.json();
+
+  const comments = commentsData.filter(item => item.productID == routeID)
 
   return {
     props: {
       product,
+      comments,
     },
-    revalidate: 60 * 60 * 24 //24 hours
+    revalidate: 60 * 60 * 12 //12 hours
   }
 }
 
